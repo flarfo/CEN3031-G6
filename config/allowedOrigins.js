ba085e2e3b9b5b7410e9b7d48ddc134a003319ad
ahhhh W <noahwhelden@gmail.com>
2024-10-20 18:23:03 -0400

    Reorganized server folder
    moved server files into server folder

--- config/allowedOrigins.js
+++
@@ -1,6 +0,0 @@
-// Define allowed origins here, these are the origins that can make API requests to the server
-const allowedOrigins = [
-    'http://localhost:3500', // local server
-];
-
-module.exports = allowedOrigins;
